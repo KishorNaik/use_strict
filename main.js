84060d99179be50463a16ede5032ffad99b784f8
Kishor Naik <kishor.naik011.net@gmail.com>
2019-05-16 16:26:44 +0530

    use strict code commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//fullName="kishor naik"; // it Will throw the exception becuase we dont have a let keyword.
+//console.log(fullName); 
+
+let fullName="Kishor Naik";
+console.log(fullName);
